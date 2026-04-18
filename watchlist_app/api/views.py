b7ca53afdc883005c1a4a67b072b531c5d7755e8
cnc-jeetu <jeetu@curvesncolors.ch>
2026-04-18 20:32:17 +0545

    Initial commit - Django DRF project

---
+++ watchlist_app/api/views.py
@@ -0,0 +1,16 @@
+from rest_framework.response import Response
+from watchlist_app.models import Movie
+from watchlist_app.api.serializers import MovieSerializer
+from rest_framework.decorators import api_view
+
+@api_view()
+def movie_list(request):
+    movies = Movie.objects.all()
+    serializer = MovieSerializer(movies, many=True)
+    return Response(serializer.data)
+
+@api_view()
+def movie_details(request, pk):
+    movies = Movie.objects.get(pk=pk)
+    serializer = MovieSerializer(movies)
+    return Response(serializer.data)
