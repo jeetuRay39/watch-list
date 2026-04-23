1b180cae422964623c0b13509a1600974db39677
jeetuRay39 <jeetu.ray39@gmail.com>
2026-04-23 16:34:45 +0545

    added the model serializers

--- watchlist_app/api/serializers.py
+++ watchlist_app/api/serializers.py
@@ -1,7 +1,21 @@
 from rest_framework import serializers
+from watchlist_app.models import Movie
 
-class MovieSerializer(serializers.Serializer):
-    id = serializers.IntegerField(read_only=True)
-    name = serializers.CharField()
-    description = serializers.CharField()
-    active = serializers.BooleanField()
+#serializer class
+# class MovieSerializer(serializers.Serializer):
+#     id = serializers.IntegerField(read_only=True)
+#     name = serializers.CharField()
+#     description = serializers.CharField()
+#     active = serializers.BooleanField()
+
+#ModelSerializer class
+
+class MovieSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Movie
+        fields = "__all__"
+        #sepecific field include
+        # fields = ['name', 'description']
+
+        #excluding the fields
+        # fields = ['name', 'description']
