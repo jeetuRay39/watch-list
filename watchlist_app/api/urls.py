0a128611834c1e73805e547b03c01fdf05ebbcf4
jeetuRay39 <jeetu.ray39@gmail.com>
2026-04-23 17:29:26 +0545

    views and the urls added for the class based api views

--- watchlist_app/api/urls.py
+++ watchlist_app/api/urls.py
@@ -1,9 +1,14 @@
 from django.urls import path, include
-from watchlist_app.api.views import movie_list, movie_details
+# from watchlist_app.api.views import movie_list, movie_details
+from watchlist_app.api.views import MovieListAPIView
 
+
+# urlpatterns = [
+
+#     path('list/', movie_list , name='movie-list'),
+#     path('<int:pk>/', movie_details, name='movie-details'),
+# ]
 
 urlpatterns = [
-
-    path('list/', movie_list , name='movie-list'),
-    path('<int:pk>/', movie_details, name='movie-details'),
+    path('list/', MovieListAPIView.as_view(), name='movie-lsit'),
 ]
--- watchlist_app/api/views.py
+++ watchlist_app/api/views.py
@@ -1,16 +1,27 @@
 from rest_framework.response import Response
+from rest_framework import status
 from watchlist_app.models import Movie
 from watchlist_app.api.serializers import MovieSerializer
-from rest_framework.decorators import api_view
+# from rest_framework.decorators import api_view
+from rest_framework.views import APIView
 
-@api_view()
-def movie_list(request):
-    movies = Movie.objects.all()
-    serializer = MovieSerializer(movies, many=True)
-    return Response(serializer.data)
+# @api_view()
+# def movie_list(request):
+#     movies = Movie.objects.all()
+#     serializer = MovieSerializer(movies, many=True)
+#     return Response(serializer.data)
 
-@api_view()
-def movie_details(request, pk):
-    movies = Movie.objects.get(pk=pk)
-    serializer = MovieSerializer(movies)
-    return Response(serializer.data)
+# @api_view()
+# def movie_details(request, pk):
+#     movies = Movie.objects.get(pk=pk)
+#     serializer = MovieSerializer(movies)
+#     return Response(serializer.data)
+
+class MovieListAPIView(APIView):
+    def get(self, request):
+        #query set for getting the objects
+        movies = Movie.objects.all()
+        #serializing the objects 
+        serializer = MovieSerializer(movies, many=True)
+        # many = Ture because we are serializing multiple objects
+        return Response(serializer.data)
